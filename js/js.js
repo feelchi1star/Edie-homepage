e3d802fdbd79ed50b1ba8d778817b32eb8441fe2
Feelchi1star_inc <feelchi1stari@localhost.localdomain>
2022-05-22 16:54:59 +0100

    Modified the Css, Html, Javascript File
    I added a sticky navbar feature to the template

--- js/js.js
+++ js/js.js
@@ -20,3 +20,18 @@ function closeNav() {
   x.style.display = "none";
 }
 close.addEventListener("click", closeNav);
+// Sticky
+window.onscroll = function () {
+  myFunction();
+};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
